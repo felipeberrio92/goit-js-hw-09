3d9b2e53ee86750d8bad21f09343900f9c581729
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-02-10 18:48:00 -0500

    se realizan las tareas del modulo 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+const inputValues = document.querySelectorAll('input');
+const startButton = document.querySelector('button');
+
+startButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  let fDelay = inputValues[0].valueAsNumber || 0;
+  let sDleay = inputValues[1].valueAsNumber || 0;
+  let stepDelay = inputValues[2].valueAsNumber || 0;
+
+  for (let i = 0; i < stepDelay; i++) {
+    createPromise(i, fDelay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    fDelay += sDleay;
+  }
+});
