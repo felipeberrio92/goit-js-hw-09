5ad4175ec93e5f983555617a922cf5279e8839aa
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-02-10 17:26:31 -0500

    se realiza punto 1 modulo 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+
+    let intervalId;
+
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+      }
+
+      function changeBackgroundColor() {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+      }
+  
+      function startChangingColor() {
+        startButton.disabled = true;
+        intervalId = setInterval(changeBackgroundColor, 1000);
+      }
+  
+      function stopChangingColor() {
+        startButton.disabled = false;
+        clearInterval(intervalId);
+      }
+  
+      startButton.addEventListener('click', startChangingColor);
+      stopButton.addEventListener('click', stopChangingColor);
